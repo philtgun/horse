47dfde971bd6acc2bbebc0872b832f59fe9ac523
Philip Tovatogan <phil.tgun@gmail.com>
2015-01-23 21:49:35 +0800

    Changed port from 80 to 3000

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ var express = require('express')
 var app = express()
 
 app.get('/', function (req, res) {
-  res.send('I\'m horse!')
+  res.send('Hello, my name is horse!')
 })
 
-var server = app.listen(80, function () {
+var server = app.listen(3000, function () {
 
   var host = server.address().address
   var port = server.address().port
