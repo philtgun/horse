046a68916c2c105403b10001cea7dc8bbfd19b26
Philip Tovstogan <phil.tgun@gmail.com>
2016-05-21 18:37:28 +0800

    Modifed logic to work with door and pir

--- horse-welcome.py
+++ horse-welcome.py
@@ -16,47 +16,51 @@ T_POLL = 0.5  # sec
 T_INSIDE_ACTIVE = 20  # sec
 T_WELCOME_DELAY = 2  # sec
 T_WELCOME_COOLDOWN = 60  # 1 min
-T_OUTSIDE_INACTIVE = 300  # 5 min
+T_DOOR_INACTIVE = 300  # 5 min
+
+PIN_PIR = 15
+PIN_DOOR = 14
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(14, GPIO.IN)
-GPIO.setup(15, GPIO.IN)
+GPIO.setup(PIN_PIR, GPIO.IN)
+GPIO.setup(PIN_DOOR, GPIO.IN)
+GPIO.setup(PIN_DOOR, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 isInBefore = False  # nobody
-isOutBefore = False  # nobody
+doorOpenBefore = False
 
 countIn = 0
-countOut = 0
+countDoor = 0
 
 print "Starting"
 
 while True:
-  isOut = GPIO.input(15)
-  isIn = GPIO.input(14)
+  doorOpen = GPIO.input(PIN_DOOR)
+  isIn = GPIO.input(PIN_PIR)
 
-  # print "[DEBUG] outside: {}, inside: {}".format(isOut, isIn)
+  print "[DEBUG] doorOpen: {}, isIn: {}, countIn: {}, countDoor {}".format(doorOpen, isIn, countIn, countDoor)
 
-  if isOut and countOut == 0:
-    print "Somebody's arriving"
-    countOut = T_INSIDE_ACTIVE / T_POLL
+  if doorOpen and not doorOpenBefore:
+    print "Somebody's opened the door"
+    countDoor = T_INSIDE_ACTIVE / T_POLL
 
   if isIn and not isInBefore:
-    if countOut > 0:
+    if countDoor > 0 and countIn == 0:
       print "Hello!"
       time.sleep(T_WELCOME_DELAY)
       greeting()
       time.sleep(T_WELCOME_COOLDOWN)
       countOut = 0
-      countIn = 0 # probably redundant
+      # countIn = 0 # probably redundant
     else:
-      print "Somebody's leaving! No hello for {} min".format(T_OUTSIDE_INACTIVE / 60)
-      countIn = T_OUTSIDE_INACTIVE / T_POLL
+      print "Somebody's leaving! No hello for {} min".format(T_DOOR_INACTIVE / 60)
+      countIn = T_DOOR_INACTIVE / T_POLL
 
   time.sleep(T_POLL)
 
   isInBefore = isIn
-  isOutBefore = isOut
+  doorOpenBefore = doorOpen
 
-  decCount(countOut)
-  decCount(countIn)
+  countDoor -= 1 if countDoor > 0 else 0
+  countIn -= 1 if countIn > 0 else 0
  
